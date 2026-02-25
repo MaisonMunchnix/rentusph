ade0041ff59583fc2646bf38cd161d97438b291a
JulietAlvarez <julietvalvarez12@gmail.com>
2026-02-25 16:17:58 +0800

    admin seeder + auth edit

--- resources/views/auth/affiliate-registration.blade.php
+++ resources/views/auth/affiliate-registration.blade.php
@@ -75,6 +75,7 @@
     <!-- Scripts -->
     <script src="{{ asset('vendor/global/global.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap-select/js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('js/custom.min.js') }}"></script>
     <script src="{{ asset('js/dlabnav-init.js') }}"></script>
 </body>
 </html>
